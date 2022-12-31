050eafb474a44154036a6d3b70dd33887d350e4b
Adesh Singh Rathore <72265093+AdeshSingh7@users.noreply.github.com>
2022-12-31 08:10:56 +0530

    Add files via upload

---
+++ tag-sync-api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import requests, csv, argparse, time, os
+
+def args():
+    parser = argparse.ArgumentParser(description='Check Fastag details through vehicle number')
+    parser.add_argument('-p', '--project', help='Project Id')
+    return parser.parse_args()
+
+def sync_tag(project_id, tag_id):
+    json = {"activate_condition":{"project_id":project_id, "tag_id":tag_id}}
+    response = requests.post('https://vehicle.parkplus.io/api/v1/sync-tag_infra/condition/', json=json)
+    status = response.json()['count']
+    if status == 0:
+        print(f"\u2713 \33[91mCheck hardware and configuration for tag_id:{tag_id} project_id:{project_id}\33[0m")
+    else:
+        print(f"\u2713 \33[92mPush cloud action for tag_id:{tag_id} on project_id:{project_id}\33[0m")
+
+def read_data():
+    project_id = args().project
+    with open("/home/"+username+"/Downloads/tags/"+project_id+".csv", "r") as csv_file:
+        data = csv.reader(csv_file)
+        for lines in data:
+            tag_id = lines[0]
+            if tag_id in "tag_id":pass
+            else:
+                try:
+                    sync_tag(project_id, tag_id)
+                    time.sleep(.1)
+                except Exception:pass
+
+try:
+    username = os.popen('users').read().strip()
+    # print(username)
+    read_data()
+except KeyboardInterrupt:pass
+except Exception:pass
+finally:pass
